feea31c3222991763b3581ff254dd65f846d6fcb
Samir Jadhav <samirzjadhav@gmail.com>
2023-08-27 22:08:55 +0530

    feat: added tags

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const colorBtn = document.getElementById("color-btn");
+const faceBtn = document.getElementById("face-btn");
+const hornsBtn = document.getElementById("horns-btn");
+const tailBtn = document.getElementById("tail-btn");
+
+const docRoot = document.querySelector(":root");
