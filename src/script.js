5f4b1229308e5cae25c018cbf24c63b0e2ba63d3
Samir Jadhav <samirzjadhav@gmail.com>
2023-08-27 22:09:52 +0530

    feat: added external  bodyColor

--- src/script.js
+++ src/script.js
@@ -4,3 +4,12 @@ const hornsBtn = document.getElementById("horns-btn");
 const tailBtn = document.getElementById("tail-btn");
 
 const docRoot = document.querySelector(":root");
+
+const bodyColor = [
+  "#a8d530",
+  "#42aaff",
+  "#f3d55b",
+  "#ff4f51",
+  "#904ae8",
+  "#ffa711",
+];
